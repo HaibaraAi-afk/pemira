c010a9035047197832415ecc5d212fa2954a61c7
Heaven Aldrico <4d3.h34v3n@gmail.com>
2024-11-13 03:31:30 +0700

    fix: update user migration

--- database/migrations/2024_11_12_134640_add_year_to_users.php
+++ database/migrations/2024_11_12_134640_add_year_to_users.php
@@ -13,7 +13,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table("users", function (Blueprint $table) {
-            $table->char("year", 2);
+            $table->char("year", 2)->after("email");
         });
         DB::update("update users set year = substring(npm, 0, 2)");
     }
