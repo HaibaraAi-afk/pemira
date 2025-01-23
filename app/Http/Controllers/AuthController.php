3e966ad28d1d7cb49a044478f1dbba965ad6c0c4
Heaven Aldrico <4d3.h34v3n@gmail.com>
2025-01-23 19:42:40 +0700

    chore: debug login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use App\Models\Whitelist;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 use Laravel\Socialite\Facades\Socialite;
 
 class AuthController extends Controller
@@ -31,6 +32,7 @@ class AuthController extends Controller
         }
 
         $npm = explode("@", $user->email)[0];
+        Log::debug("NPM: {$npm} {$user->email}");
         if (!Whitelist::query()->find($npm)) {
             return redirect(route("login"))
                 ->with("flash.message", "Maaf.. NPM kamu tidak terdaftar sebagai pemilih 😔")
